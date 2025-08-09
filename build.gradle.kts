45d2559e1e454f3a69a6349f325a28ad2886cb67
maikotrindade <maikocharles@gmail.com>
2025-08-09 12:08:06 -0600

    removed ktor code related

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,14 @@
-val kotlin_version: String by project
 val logback_version: String by project
 val koog_agents_version: String by project
 val dotenv_kotlin_version: String by project
-val ktor_version: String by project
 
 plugins {
     kotlin("jvm") version "2.1.10"
     kotlin("plugin.serialization") version "2.1.10"
-    id("io.ktor.plugin") version "3.1.3"
 }
 
 group = "io.github.maikotrindade"
 version = "0.0.1"
-
-application {
-    mainClass = "io.ktor.server.netty.EngineMain"
-}
 
 repositories {
     mavenCentral()
@@ -26,15 +19,6 @@ kotlin {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core:3.1.3")
-    implementation("io.ktor:ktor-server-openapi:3.1.3")
-    implementation("io.ktor:ktor-server-swagger:3.1.3")
-    implementation("io.ktor:ktor-server-call-logging:3.1.3")
-    implementation("io.ktor:ktor-server-netty:3.1.3")
-    implementation("io.ktor:ktor-server-config-yaml:3.1.3")
-    implementation("io.ktor:ktor-server-content-negotiation:3.1.3")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:3.1.3")
-
     implementation("ch.qos.logback:logback-classic:${logback_version}")
 
     implementation("ai.koog:koog-agents:$koog_agents_version")
--- src/main/kotlin/server/model/AgentRequest.kt
+++
@@ -1,6 +0,0 @@
-package server.model
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class AgentRequest(val goal: String, val steps: List<String>)
