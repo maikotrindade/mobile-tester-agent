e94ad929e2d16a580329ccb521d4b73c689d8e74
maikotrindade <maikocharles@gmail.com>
2025-08-09 13:52:34 -0600

    remove simple agent

--- src/main/kotlin/server/Routing.kt
+++ src/main/kotlin/server/Routing.kt
@@ -1,7 +1,6 @@
 package server
 
 import agent.ComplexTesterAgent
-import agent.TesterAgent
 import agent.executor.*
 import io.ktor.http.*
 import io.ktor.server.application.*
@@ -16,40 +15,10 @@ fun Application.configureRouting() {
     routing {
         staticResources("/openapi", "openapi")
         swaggerUI(path = "/swagger", swaggerFile = "openapi/openapi.yaml")
-        agentPost("/gemini") { GeminiExecutor() }
-        agentPost("/ollama/gwen") { OllamaGwenExecutor() }
-        agentPost("/ollama/llama") { OllamaGwenExecutor() }
-        agentPost("/openRouter") { OpenRouterExecutor() }
         complexAgentPost("/gemini/complex") { GeminiExecutor() }
         complexAgentPost("/ollama/gwen/complex") { OllamaGwenExecutor() }
         complexAgentPost("/ollama/llama/complex") { OllamaLlamaExecutor() }
         complexAgentPost("/openRouter/complex") { OpenRouterExecutor() }
-    }
-}
-
-fun <T> Route.agentPost(path: String, executorProvider: () -> T) where T : Any {
-    post(path) {
-        try {
-            val request = call.receive<AgentRequest>()
-            val goal = request.goal
-            val stepsAsStrings = request.steps
-            if (goal.isBlank()) {
-                return@post call.respondText(
-                    "Missing goal",
-                    status = HttpStatusCode.BadRequest
-                )
-            }
-            if (stepsAsStrings.isEmpty()) {
-                return@post call.respondText(
-                    "Missing steps",
-                    status = HttpStatusCode.BadRequest
-                )
-            }
-            val result = TesterAgent.runAgent(goal, stepsAsStrings, executorProvider() as ExecutorInfo)
-            call.respond(result)
-        } catch (e: Exception) {
-            call.respondText("Error: ${e::class.simpleName}: ${e.message}", status = HttpStatusCode.BadRequest)
-        }
     }
 }
 
