e0009401f6c5db3f5fecd7507524143a82274ae1
maikotrindade <maikocharles@gmail.com>
2025-08-16 11:59:38 -0600

    moved Android sample app to its own repository

--- android/app/src/main/java/io/githib/maikotrindade/appfortesting/model/ChatMessage.kt
+++
@@ -1,3 +0,0 @@
-package io.githib.maikotrindade.appfortesting.model
-
-data class ChatMessage(val text: String, val isMe: Boolean)
--- android/app/src/main/java/io/githib/maikotrindade/appfortesting/model/Story.kt
+++
@@ -1,7 +0,0 @@
-package io.githib.maikotrindade.appfortesting.model
-
-data class Story(
-    val user: User,
-    val hasStory: Boolean = true,
-    val isLive: Boolean = false
-)
